e417fcda0bf974220b349504a7abb04af6a98945
Vishal Raj <vishalraj82@gmail.com>
2019-08-29 07:20:21 +0400

    Almost working, with sample sql

--- scripts/mysql-setup-replication.sh
+++ scripts/mysql-setup-replication.sh
@@ -2,84 +2,64 @@
 
 set -x
 
+# Add a wait for containers to be ready
+sleep 5
+
 # Install the mysql client
-apt-get update
+apt-get -qq update
 apt-get install mysql-client -y --no-install-recommends
 
-# Add a wait for containers to be ready
-sleep 15
-
-# Create a user on master node
-mysql \
-    -u "$MASTER_ROOT_USER" \
-    -p"$MASTER_ROOT_PASSWORD" \
-    -h"$MASTER_HOSTNAME" \
-    -AN \
+Create a user on master node
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
     -e "CREATE USER '$SLAVE_REPL_USER'@'$SLAVE_HOSTNAME' IDENTIFIED BY '$SLAVE_REPL_PASSWORD';"
 
 # Grant replication permission to the user on master node
-mysql \
-    -u "$MASTER_ROOT_USER" \
-    -p"$MASTER_ROOT_PASSWORD" \
-    -h"$MASTER_HOSTNAME" \
-    -AN \
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
     -e "GRANT REPLICATION SLAVE ON *.* TO '$SLAVE_REPL_USER'@'$SLAVE_HOSTNAME' IDENTIFIED BY '$SLAVE_REPL_PASSWORD';"
 
 # Reload privileges for replication to take effect
-mysql \
-    -u "$MASTER_ROOT_USER" \
-    -p"$MASTER_ROOT_PASSWORD" \
-    -h"$MASTER_HOSTNAME" \
-    -AN \
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
     -e "FLUSH PRIVILEGES;"
 
 # See the privileges for replication user
-mysql \
-    -u "$MASTER_ROOT_USER" \
-    -p"$MASTER_ROOT_PASSWORD" \
-    -h"$MASTER_HOSTNAME" \
-    -AN \
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
     -e "SHOW GRANTS FOR '$SLAVE_REPL_USER'@'$SLAVE_HOSTNAME';"
 
 
 # Dump the replication database to be uploaded in slave node
-mysqldump
-    -u "$MASTER_ROOT_USER" \
-    -p"$MASTER_ROOT_PASSWORD" \
-    -h"$MASTER_HOSTNAME" \
+mysqldump --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
     --add-drop-database \
+    --add-drop-table \
     "$MASTER_REPL_DB" > "$MASTER_REPL_DB.sql"
+
+cat "$MASTER_REPL_DB.sql"
+
     
+# Dump replication data in master
+# mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME"  < "$MASTER_REPL_DB.sql"
 
-# Import replication database in slave
-mysql \
-    -u  "$SLAVE_ROOT_USER" \
-    -p "$SLAVE_ROOT_PASSWORD" \
-    -h "$SLAVE_HOSTNAME" < "$MASTER_REPL_DB.sql"
+# # Import replication database in slave
+# mysql --user="$SLAVE_ROOT_USER" --password= "$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" < cat "$MASTER_REPL_DB.sql"
 
-REPL_LOG_FILE=$(eval "mysql -u $MASTER_ROOT_USER -p$MASTER_ROOT_PASSWORD -h $MASTER_HOSTNAME -e 'SHOW MASTER STATUS\G' | grep File | sed -n -e 's/^.*: //p'")
-REPL_LOG_POSITION=$(eval "mysql -u $MASTER_ROOT_USER -p$MASTER_ROOT_PASSWORD -h $MASTER_HOSTNAME -e 'SHOW MASTER STATUS\G' | grep Position | sed -n -e 's/^.*: //p'")
+# REPL_LOG_FILE=$(eval "mysql --user=$MASTER_ROOT_USER --password=$MASTER_ROOT_PASSWORD --host=$MASTER_HOSTNAME -e 'SHOW MASTER STATUS\G' | grep File | sed -n -e 's/^.*: //p'")
+# REPL_LOG_POSITION=$(eval "mysql --user=$MASTER_ROOT_USER --password=$MASTER_ROOT_PASSWORD --host=$MASTER_HOSTNAME -e 'SHOW MASTER STATUS\G' | grep Position | sed -n -e 's/^.*: //p'")
 
+# QUERY="CHANGE MASTER TO MASTER_HOST = '$MASTER_HOSTNAME', MASTER_USER = '$SLAVE_REPL_USER', MASTER_PASSWORD = '$SLAVE_REPL_PASSWORD', MASTER_LOG_FILE = '$REPL_LOG_FILE', MASTER_LOG_POS = $REPL_LOG_POSITION;"
 
-mysql \
-    -u "$MASTER_ROOT_USER" \
-    -p"$MASTER_ROOT_PASSWORD" \
-    -h"$MASTER_HOSTNAME" \
-    -AN \
-    -e "CHANGE MASTER TO MASTER_HOST='$MASTER_HOSTNAME',MASTER_USER='$SLAVE_REPL_USER', MASTER_PASSWORD='$SLAVE_REPL_PASSWORD', MASTER_LOG_FILE='$REPL_LOG_FILE', MASTER_LOG_POS='$REPL_LOG_POSITION';"
+# mysql --user="SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" \
+#     -e $QUERY
 
-# Start the slave for replication
-mysql \
-    -u "$MASTER_ROOT_USER" \
-    -p"$MASTER_ROOT_PASSWORD" \
-    -h"$MASTER_HOSTNAME" \
-    -AN \
-    -e "START SLAVE;"
+# # Start the slave for replication
+# mysql --user="SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" \
+#     -e "START SLAVE;"
 
-# See the slave status
-mysql \
-    -u "$MASTER_ROOT_USER" \
-    -p"$MASTER_ROOT_PASSWORD" \
-    -h"$MASTER_HOSTNAME" \
-    -AN \
-    -e "SHOW SLAVE STATUS;"
+# # See the slave status
+# mysql --user="SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" \
+#     -e "SHOW SLAVE STATUS \G;"
+
+# # Dump sample data in master
+# mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" < cat /opt/scripts/Demo-1K-Rows.sql
+
+# # See the slave status after dump
+# mysql --user="SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" \
+#     -e "SHOW SLAVE STATUS \G;"
--- start.sh
+++ start.sh
@@ -7,14 +7,39 @@ trap ctrl_c INT
 
 function ctrl_c() {
     docker-compose down --remove-orphans
+    docker-compose ps
+    #docker volume ls | awk '{ print $2 }' | grep "^[a\-z0\-9]\+" | xargs docker volume rm
 }
 
-if [ "$1" == "clean" ]
+arg1=$1
+arg2=$1
+
+if [ "$arg1" == "clean" || "$arg1" == "cleanup"]
 then
-    for dir in "master" "slave"
+    arg2=$2
+fi
+
+if [ "${arg1:0:5}" == "clean" ]
+then
+    for par in "master" "slave"
     do
-        sudo rm -rf $dir/var/lib/mysql/* $dir/var/log/mysql/*
+        for chi in "var/lib/" "var/log" "var/run"
+        do
+            chd=$chi/mysql
+            sudo rm -rf $par/$chd/* $par/$chd/* $par/$chd/*
+        done
     done
+fi
+
+if [ "$arg1" == "cleanup" ]
+then
+    exit 0;
+fi
+
+if [ "$arg2" == "setup" ]
+then
+    docker-compose up mysql_repliation_setup
+    exit 0;
 fi
 
 docker-compose up --force-recreate
