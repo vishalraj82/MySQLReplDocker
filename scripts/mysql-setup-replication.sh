2af0582467881f79ea1d352ba42540bef77df6ee
Vishal Raj <vishalr@one.com>
2019-08-29 13:09:29 +0400

    Replication has started working. Needs clean up now

--- scripts/mysql-setup-replication.sh
+++ scripts/mysql-setup-replication.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#set -x
+set -x
 
 # Add a wait for containers to be ready
 #sleep 5
@@ -9,68 +9,52 @@
 apt-get -qq update
 apt-get -qq install mysql-client -y --no-install-recommends
 
-# # Create a user on master node
-# mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
-#     -e "CREATE USER '$SLAVE_REPL_USER'@'$SLAVE_HOSTNAME' IDENTIFIED BY '$SLAVE_REPL_PASSWORD';"
+# Create a user on master node
+MYSQL_QUERY="CREATE USER '$SLAVE_REPL_USER'@'$SLAVE_HOST_IP' IDENTIFIED BY '$SLAVE_REPL_PASSWORD';"
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOST_IP" -e "$MYSQL_QUERY"
 
 # Grant replication permission to the user on master node
-echo "Granting permssion on master for replication"
-mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
-    -e "GRANT REPLICATION SLAVE ON *.* TO '$SLAVE_REPL_USER'@'$SLAVE_HOSTNAME' IDENTIFIED BY '$SLAVE_REPL_PASSWORD';"
+MYSQL_QUERY="GRANT REPLICATION SLAVE ON *.* TO \"$SLAVE_REPL_USER\"@\"$SLAVE_HOST_IP\" IDENTIFIED BY \"$SLAVE_REPL_PASSWORD\";"
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOST_IP" -AN -e "$MYSQL_QUERY"
 
 # Reload privileges for replication to take effect
-echo "Flushing privileges for replication permission to take effect"
-mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
-    -e "FLUSH PRIVILEGES;"
+MYSQL_QUERY="FLUSH PRIVILEGES;"
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOST_IP" -e "$MYSQL_QUERY"
 
 # See the privileges for replication user
-echo "Flushing tables with read lock"
-mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
-    -e "FLUSH TABLES WITH READ LOCK;" 
+MYSQL_QUERY="FLUSH TABLES WITH READ LOCK;"
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOST_IP" -e "$MYSQL_QUERY"
 
 # Dump the replication database to be uploaded in slave node
-echo "Dumping all database from master"
-mysqldump --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
-    --all-database \
-    --add-drop-database \
-    --add-drop-table > /tmp/master.dump.sql
+mysqldump --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOST_IP" --all-database --add-drop-database --add-drop-table > /tmp/master.dump.sql
 
 ls -l /tmp/*.sql
 
 # Unlock the tables on master
-echo "Flushing tables with read lock"
-mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" \
-    -e "UNLOCK TABLES;"
+MYSQL_QUERY="UNLOCK TABLES;"
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOST_IP" -e "$MYSQL_QUERY;"
     
-# # Import replication database in slave
-echo "Importing dump from master to slave"
-mysql --user="$SLAVE_ROOT_USER" --password= "$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" < cat /tmp/master.dump.sql
+# Import replication database in slave
+mysql --user="$SLAVE_ROOT_USER" --password= "$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOST_IP" < cat /tmp/master.dump.sql
 
-REPL_LOG_FILE=$(eval "mysql --user=$MASTER_ROOT_USER --password=$MASTER_ROOT_PASSWORD --host=$MASTER_HOSTNAME -e 'SHOW MASTER STATUS\G' | grep File | sed -n -e 's/^.*: //p'")
-REPL_LOG_POSITION=$(eval "mysql --user=$MASTER_ROOT_USER --password=$MASTER_ROOT_PASSWORD --host=$MASTER_HOSTNAME -e 'SHOW MASTER STATUS\G' | grep Position | sed -n -e 's/^.*: //p'")
+REPL_LOG_FILE=$(eval "mysql --user=$MASTER_ROOT_USER --password=$MASTER_ROOT_PASSWORD --host=$MASTER_HOST_IP -e 'SHOW MASTER STATUS\G' | grep File | sed -n -e 's/^.*: //p'")
+REPL_LOG_POSITION=$(eval "mysql --user=$MASTER_ROOT_USER --password=$MASTER_ROOT_PASSWORD --host=$MASTER_HOST_IP -e 'SHOW MASTER STATUS\G' | grep Position | sed -n -e 's/^.*: //p'")
 
-QUERY="CHANGE MASTER TO MASTER_HOST = '$MASTER_HOSTNAME', MASTER_USER = '$SLAVE_REPL_USER', MASTER_PASSWORD = '$SLAVE_REPL_PASSWORD', MASTER_LOG_FILE = '$REPL_LOG_FILE', MASTER_LOG_POS = $REPL_LOG_POSITION;"
+MYSQL_QUERY="CHANGE MASTER TO MASTER_HOST = '$MASTER_HOST_IP', MASTER_USER = '$SLAVE_REPL_USER', MASTER_PASSWORD = '$SLAVE_REPL_PASSWORD', MASTER_LOG_FILE = '$REPL_LOG_FILE', MASTER_LOG_POS = $REPL_LOG_POSITION;"
 
-# echo "Switching master on slave machine"
-echo *************************************************************************************
-echo $QUERY
-echo *************************************************************************************
-mysql --user="$SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" \
-    -e $QUERY
+mysql --user="$SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOST_IP" -e "$MYSQL_QUERY"
 
-# # Start the slave for replication
-# echo "Starting slave"
-# mysql --user="$SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" \
-#     -e "START SLAVE;"
+# Start the slave for replication
+MYSQL_QUERY="START SLAVE;"
+mysql --user="$SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOST_IP" -e "$MYSQL_QUERY"
 
-# # See the slave status
-# echo "Checking slave status"
-# mysql --user="$SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" \
-#     -e "SHOW SLAVE STATUS \G;"
+# See the slave status
+MYSQL_QUERY="SHOW SLAVE STATUS \G;"
+mysql --user="$SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOST_IP" -e "$MYSQL_QUERY"
 
-# # Dump sample data in master
-# mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOSTNAME" < cat /opt/scripts/Demo-1K-Rows.sql
+# Dump sample data in master
+mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOST_IP" < cat /opt/scripts/Demo-1K-Rows.sql
 
-# # See the slave status after dump
-# mysql --user="SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOSTNAME" \
-#     -e "SHOW SLAVE STATUS \G;"
+# See the slave status after dump
+MYSQL_QUERY="SHOW SLAVE STATUS \G;"
+mysql --user="$SLAVE_ROOT_USER" --password="$SLAVE_ROOT_PASSWORD" --host="$SLAVE_HOST_IP" -e "$MYSQL_QUERY"
